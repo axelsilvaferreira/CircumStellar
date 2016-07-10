4c3f397c8b9a4a777972c3154b139b16404455f9
Axel Ferreira <axelsilvaferreira@gmail.com>
2016-07-10 02:41:28 +0100

    correcção do GameOver

---
+++ GameOverViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GameOverViewController.swift
+//  CircumStellar
+//
+//  Created by Axel Ferreira on 10/07/16.
+//  Copyright © 2016 Axel Ferreira. All rights reserved.
+//
+
+import UIkit
+import SpriteKit
+
+class GameOverViewController: SKScene {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
